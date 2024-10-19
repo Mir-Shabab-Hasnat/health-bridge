3dd2785a098df17f18aaabea06d1169234f3524e
Logan Fossenier <logan.fossenier@gmail.com>
2024-10-19 00:26:01 -0600

    feat: add patient specific appointment fetching

--- convex/queries/appointmentFetching.ts
+++ convex/queries/appointmentFetching.ts
@@ -1,19 +1,38 @@
 "use server";
 
-import { ConvexError } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 
 import { query } from '../_generated/server';
 
 export const getAllAppointments = query({
   handler: async (ctx) => {
-    const appointments = await ctx.db.query("appointment");
+    const allAppointments = await ctx.db.query("appointment");
 
-    if (!appointments) {
+    if (!allAppointments) {
       throw new ConvexError({
         message: "Server side error fetching appointments.",
       });
     }
 
-    return appointments;
+    return allAppointments;
+  },
+});
+
+export const getPatientAppointments = query({
+  args: {
+    userId: v.id("user"),
+  },
+  handler: async (ctx, args) => {
+    const userAppointments = await ctx.db
+      .query("appointment")
+      .filter((q) => q.eq(q.field("patient"), args.userId));
+
+    if (!userAppointments) {
+      throw new ConvexError({
+        message: "Server side error fetching appointments.",
+      });
+    }
+
+    return userAppointments;
   },
 });
