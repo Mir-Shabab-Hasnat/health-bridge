4e5b291245e45567b96d61c675c086185cc694c7
Anna Valentino <pusheen1308@gmail.com>
2024-10-18 22:14:17 -0600

    got all the page connect together in patient side, just need to implement functions

---
+++ app/form/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import FormContainer from "./FormContainer";
+import ChatContainer from "./ChatContainer";
+
+
+const FormAndChat = () => {
+    return(
+        <div>
+            <div>
+                <FormContainer />
+            </div>
+
+
+            <div>
+                <ChatContainer />
+            </div>
+        </div>
+    );
+}
+
+
+export default FormAndChat;
