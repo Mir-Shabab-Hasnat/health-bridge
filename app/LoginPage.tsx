1248630f5e80993841061d17f2f909565b28860c
Logan Fossenier <logan.fossenier@gmail.com>
2024-10-19 13:56:40 -0600

    push

--- app/LoginPage.tsx
+++ app/LoginPage.tsx
@@ -1,113 +1,135 @@
 "use client";
 
-import * as z from "zod";
-import {Button} from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useMutation } from "convex/react";
+import { NextResponse } from "next/server";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
-
-
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const formSchema = z.object({
-    username: z.string().min(6),
-    password: z.string().min(8),
+  username: z.string().min(6),
+  password: z.string().min(8),
 });
 
-
 const LoginPage = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-            password: "",
-        },
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
-    const authenticate = useMutation(api.mutations.userAuthentication.authenticate);
+  const authenticate = useMutation(
+    api.mutations.userAuthentication.authenticate
+  );
 
-    const handleSubmit = async (data : z.infer<typeof formSchema>) => {
-        try {
-            console.log("Form Data: ", data);
-            console.log(data)
-            const result: boolean | (string | boolean)[] = await authenticate({ username: data.username, password: data.password });
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      console.log("Form Data: ", data);
+      console.log(data);
+      const result: boolean | (string | boolean)[] = await authenticate({
+        username: data.username,
+        password: data.password,
+      });
 
+      if (Array.isArray(result)) {
+        console.log("result", result);
+        const [userId, isDoctor] = result;
 
-            if (Array.isArray(result)) {
-                console.log("result", result)
-                const [userId, isDoctor] = result;
-
-                if (typeof userId === 'string') {
-                    document.cookie = `userId=${userId}; path=/`;
-                }
-
-                if (typeof isDoctor === 'boolean') {
-                    document.cookie = `isDoctor=${isDoctor}; path=/`;
-                }
-
-            } else if (typeof result === 'boolean') {
-                document.cookie = `loginSuccess=${result}; path=/`;
-            }
-
-            alert('User registered successfully!');
-        } catch (error) {
-            console.error(error);
-            alert('Error registering user: ' + error)
+        if (typeof userId === "string") {
+          document.cookie = `userId=${userId}; path=/`;
         }
-    };
 
-    return (
-        <div className="form-auth">
-            <div className="pre-form-text">
-                <p>Welcome to health-bridge</p>
-                <h1>Sign in</h1>
-                <p>Not have an account with us? <a href="https://www.google.com/">Create one</a></p>
-            </div>
+        if (typeof isDoctor === "boolean") {
+          document.cookie = `isDoctor=${isDoctor}; path=/`;
+        }
+      } else if (typeof result === "boolean") {
+        document.cookie = `loginSuccess=${result}; path=/`;
+      }
 
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} className="w-full flex flex-col gap-4">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="your username" type="username" {...field} />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+      NextResponse.redirect(
+        "https://health-bridge-ten.vercel.app/patient-dashboard"
+      );
+    } catch (error) {
+      console.error(error);
+      alert("Error registering user: " + error);
+    }
+  };
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="your super secret password" type="password" {...field} />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+  return (
+    <div className="form-auth">
+      <div className="pre-form-text">
+        <p>Welcome to health-bridge</p>
+        <h1>Sign in</h1>
+        <p>
+          Not have an account with us?{" "}
+          <a href="https://www.google.com/">Create one</a>
+        </p>
+      </div>
 
-                    <div className="flex gap-2">
-                        <Button type="submit" className="secondary-btn">
-                            Submit
-                        </Button>
-                    </div>
-                </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="w-full flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="your username"
+                    type="username"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            </Form>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="your super secret password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        </div>
-    )
-}
-
+          <div className="flex gap-2">
+            <Button type="submit" className="secondary-btn">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginPage;
