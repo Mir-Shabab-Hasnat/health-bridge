56f783f2713d43e781f352674c404614d2ae6032
Logan Fossenier <logan.fossenier@gmail.com>
2024-10-19 03:15:50 -0600

    feat: get convex data working in the frontend

--- app/prodtest/page.tsx
+++ app/prodtest/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useQuery } from 'convex/react';
-import { useState } from 'react';
 
 import { api } from '@/convex/_generated/api';
 
@@ -10,30 +9,25 @@ const AppointmentPage = () => {
     api.queries.appointmentFetching.getAllAppointments
   );
 
-  const [appointments] = useState(appointmentData);
+  // console.log("point a");
+
+  // const [appointments, setAppointments] =
+  //   useState<typeof appointmentData>(undefined);
+  // setAppointments(appointmentData);
+
+  // console.log(appointmentData);
+
+  // console.log("point b");
+
+  // console.log(appointments?.at(0));
 
   return (
     <div>
-      {appointments?.map((appointment, idx) => (
+      {appointmentData?.map((appointment, idx) => (
         <p key={idx}>{appointment.patient}</p>
       ))}
       <p>hi</p>
     </div>
-    // {row.map((tile, tileIndex) => (
-    //   <div
-    //     key={rowIndex * 25 + tileIndex}
-    //     className={`w-1/25 border-2 border-gray-300 box-border cursor-pointer text-sm overflow-hidden whitespace-nowrap font-bold text-white ${
-    //       tile === "x"
-    //         ? "bg-red-600"
-    //         : tile === " " || tile === "door"
-    //           ? "bg-yellow-200"
-    //           : "bg-black"
-    //     }`}
-    //     onClick={() => onTileClick(rowIndex * 25 + tileIndex)}
-    //   >
-    //     {tile !== "x" && tile !== " " ? tile : ""}
-    //   </div>
-    // ))}
   );
 };
 
