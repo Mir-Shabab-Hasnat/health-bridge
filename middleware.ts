d0e24fe036e9ee8f2acac2b374b04383d80bcf24
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-10-19 10:03:24 -0600

    chat mutation good

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,24 @@
-import { NextResponse, type NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("userId")?.value;
-  const isDoctor = request.cookies.get("isDoctor")?.value === "true"; // Assuming this is a string that indicates true/false.
 
-  console.log('Current User:', currentUser);
-  console.log('Is Doctor:', isDoctor);
-  console.log('Current Path:', request.nextUrl.pathname);
+  const currentUser = request.cookies.get("currentUser")?.value;
+  const isDoctor = request.cookies.get("isDoctor")?.value;
 
-  // Redirect to home if the user is not logged in and not on the home page
-  if (!currentUser && !request.nextUrl.pathname.startsWith("/")) {
-    return NextResponse.redirect(new URL("/", request.url));
+
+  if (!currentUser && !request.nextUrl.pathname.startsWith("/home")) {
+    return Response.redirect(new URL("/home", request.url));
   }
 
-  // Redirect to the doctor dashboard if the user is a doctor and not on the correct page
   if (currentUser && isDoctor && !request.nextUrl.pathname.startsWith("/doctor-dashboard")) {
-    return NextResponse.redirect(new URL("/doctor-dashboard", request.url));
+    return Response.redirect(new URL("/doctor-dashboard", request.url));
   }
 
-  // Redirect to the patient dashboard if the user is not a doctor and not on the correct page
   if (currentUser && !isDoctor && !request.nextUrl.pathname.startsWith("/patient-dashboard")) {
-    return NextResponse.redirect(new URL("/patient-dashboard", request.url));
+    return Response.redirect(new URL("/patient-dashboard", request.url));
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/', '/:path*'], // Match all paths to apply middleware globally
+  matcher: ["/((?!api|_next/static|_next/image|.\.png$).)"],
 };
